27f54a3d93596b8c00f4fe69323b368a150f4ea7
André Baldeweg <a@baldeweg.org>
2022-09-08 09:38:49 +0000

    trim product name and unit

--- src/composables/useInventory.js
+++ src/composables/useInventory.js
@@ -6,11 +6,11 @@ export function useInventory() {
 
   onMounted(() => {
     import.meta.env.VUE_APP_PRODUCTS.split(',').forEach((element, index) => {
-      products.value.push({ key: index, value: element })
+      products.value.push({ key: index, value: element.trim() })
     })
 
     import.meta.env.VUE_APP_UNITS.split(',').forEach((element, index) => {
-      units.value.push({ key: index, value: element })
+      units.value.push({ key: index, value: element.trim() })
     })
   })
 
